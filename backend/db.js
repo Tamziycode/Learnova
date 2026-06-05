be8fbd90baf426cb01fcd18d8a379e71ad446160
Tamziy <tamziyafolabi@gmail.com>
2026-06-05 08:00:12 -0400

    fix: database connection ports and ssl

--- backend/db.js
+++ backend/db.js
@@ -3,9 +3,14 @@ const mysql = require("mysql2");
 // connection pool creation
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT, // Tells it to look at door 24288
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
+  ssl: {
+    // Explicitly handles Aiven's security requirement
+    rejectUnauthorized: false,
+  },
 });
 
 // Export promise-based connection
